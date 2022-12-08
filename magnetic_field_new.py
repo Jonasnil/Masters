5f9c55f264f6164c152a3761225e5d33f9d7347d
Jonasnil <xit_nil@hotmail.com>
2022-12-08 23:36:21 +0100

    changes and new files

--- magnetic_field_new.py
+++ magnetic_field_new.py
@@ -11,6 +11,15 @@ import matplotlib.pyplot as plt
 class MagneticField:
     def __init__(self, time_array, cell=None, rlc_type='under'):
         self.cell = cell                                    # Cell object
+        self.children_dict = {}                             # Dict, children sections.
+        self.parent_connection_dict = self.cell.get_dict_parent_connections()  # Dict, conn to parent in float [0, 1].
+        self.parent_connection_idx = {}                     # Dict, seg idx of parent.
+        self.has_children = {}                              # Dict, boolean.
+        self.cell_idx_dict = {}                             # Dict, all seg idx per sec.
+        self.par_child_vec_dict = {}                        # Dict, vec p-c connections.
+        self.par_child_center_point = {}                    # Dict, center coord between parent and child
+        self.par_child_l_c_dict = {}                        # Dict, l between center of p-c connections.
+        self.par_child_r_a = {}                             # Dict, axial resistance for p-c connections.
         self.time_array = time_array                        # Time array [ms]
         self.timestep = self.cell.dt                        # Timestep [ms]
         self.rlc_type = rlc_type                            # Over- or under-dampened rlc circuit
@@ -35,7 +44,7 @@ class MagneticField:
                           'cpd': 0.01                       # Distance from the coil to the plane of the E-field [m]
                           }
         self.rlc_circuit_data_over = {'type': 'over',       # Type of stimuli (over-dampened)
-                                      'V0': 30,             # Initial charge of capacitor [V]
+                                      'V0': 7500,           # Initial charge of capacitor [V]
                                       'R': 3,               # Resistance [ohm]
                                       'C': 200 * 10**-6,    # Conductance [F]
                                       'L': 165 * 10**-6     # Inductance in [H]
@@ -48,8 +57,8 @@ class MagneticField:
                                        }
 
         self.E_t, self.I_coil = self._temporal_field()
-        self.magnetic_field_magnitude = ((np.max(self.I_coil) * self.coil_data['u'] * self.coil_data['N'])
-                                         / (2. * self.coil_data['r_c']))
+        self.magnetic_flux_density = ((np.max(self.I_coil) * self.coil_data['u'] * self.coil_data['N'])
+                                      / (2. * self.coil_data['r_c']))
 
     def _calc_omega_1(self, R, L):
         return R / (2. * L)
@@ -100,6 +109,98 @@ class MagneticField:
 
         return E_t, I_coil
 
+    def _get_local_parent_connection_idx(self, parent_name, child_name):
+        num_of_seg_in_sec = len(self.cell_idx_dict[parent_name])
+        parent_connection = self.parent_connection_dict[child_name]
+        if parent_connection == 0:
+            return parent_connection
+        else:
+            return m.ceil(parent_connection * num_of_seg_in_sec) - 1
+
+    def _create_cell_structure_dicts(self):
+        seg_idx = 0
+        for sec in self.cell.allseclist:
+            sec_name = sec.name()
+            list_of_seg_idx_in_sec = []
+            for _ in sec:
+                list_of_seg_idx_in_sec.append(seg_idx)
+                seg_idx += 1
+            self.cell_idx_dict[sec_name] = list_of_seg_idx_in_sec
+
+            child_list = []
+            for child in neuron.h.SectionRef(sec=sec).child:
+                child_list.append(child)
+            self.children_dict[sec_name] = child_list
+            if len(child_list) > 0:
+                self.has_children[sec_name] = True
+            else:
+                self.has_children[sec_name] = False
+
+        for sec in self.cell.allseclist:
+            sec_name = sec.name()
+            if self.has_children[sec_name]:
+                for child in self.children_dict[sec_name]:
+                    child_name = child.name()
+                    local_parent_idx = self._get_local_parent_connection_idx(sec_name, child_name)
+                    self.parent_connection_idx[child_name] = self.cell_idx_dict[sec_name][local_parent_idx]
+
+    def _calc_multisec_data(self):
+        for i in range(self.cell.totnsegs):
+            self.c_comp_coords[i] = (self.comp_coords[i] + self.comp_coords[i + 1]) * 0.5
+        for sec in self.cell.allseclist:
+            sec_name = sec.name()
+            sec_idx = self.cell_idx_dict[sec_name]
+            for i in sec_idx[:-1]:
+                self.c_comp_vecs[i] = self.c_comp_coords[i + 1] - self.c_comp_coords[i]
+
+            if self.has_children[sec_name]:
+                for child in self.children_dict[sec_name]:
+                    child_name = child.name()
+                    child_idx = self.cell_idx_dict[child_name][0]
+                    parent_idx = self.parent_connection_idx[child_name]
+                    #                    if sec_name == 'soma[0]':
+                    #                        if self.parent_connection_dict[sec_name] == 0.5:
+                    #                            + 1-0.5 * soma_length ...
+
+                    center_vec = self.c_comp_coords[child_idx] - self.c_comp_coords[parent_idx]
+                    self.par_child_vec_dict[child_name] = center_vec
+                    self.par_child_center_point[child_name] = self.c_comp_coords[parent_idx] + center_vec * 0.5
+                    l_c = m.sqrt(center_vec[0] ** 2 + center_vec[1] ** 2 + center_vec[2] ** 2)
+                    self.par_child_l_c_dict[child_name] = l_c
+                    self.par_child_r_a[child_name] = self.r_a[parent_idx + 1] + self.r_a[child_idx]
+
+    def _calc_input_current_multisec(self):
+        for sec in self.cell.allseclist:
+            sec_name = sec.name()
+            sec_idx = self.cell_idx_dict[sec_name]
+            between_comps_coord = self.comp_coords[sec_idx[0]:sec_idx[-1]]
+            if np.size(between_comps_coord) > 0:
+                E_s_value, E_dir = self._spatial_field(between_comps_coord)
+                for seg_idx in self.cell_idx_dict[sec_name][:-1]:
+                    E_idx = seg_idx - self.cell_idx_dict[sec_name][0]
+                    c_vec = self.c_comp_vecs[seg_idx]
+                    for i in range(len(self.time_array)):
+                        E_value = E_s_value[E_idx] * self.E_t[i]
+                        I_a = self._calc_I_a_ctc(E_value, E_dir[E_idx], c_vec * 10**-6, self.r_a[seg_idx + 1])
+                        self.input_current[seg_idx, i] += -I_a
+                        self.input_current[seg_idx + 1, i] += I_a
+
+            if self.has_children[sec_name]:
+                for child in self.children_dict[sec_name]:
+                    child_name = child.name()
+                    parent_idx = self.parent_connection_idx[child_name]
+                    child_idx = self.cell_idx_dict[child_name][0]
+                    c_vec = self.par_child_vec_dict[child_name]
+                    r_a_pc = self.par_child_r_a[child_name]
+                    between_comps_coord = self.par_child_center_point[child_name]
+                    E_s_value, E_dir = self._spatial_field(between_comps_coord)
+
+                    for i in range(len(self.time_array)):
+                        E_value = E_s_value * self.E_t[i]
+                        I_a = self._calc_I_a_ctc(E_value, E_dir, c_vec * 10**-6, r_a_pc)
+                        self.input_current[parent_idx, i] += -I_a
+                        self.input_current[child_idx, i] += I_a
+
     def _calc_m_kk(self, r_c, rho, z):
         return (4. * r_c * rho) / ((r_c + rho)**2 + z**2)
 
@@ -108,41 +209,59 @@ class MagneticField:
 
     def _spatial_field(self, input_array):
         # input_array [um]
-        num_of_coords = len(input_array[0])
         N = self.coil_data['N']
         r_c = self.coil_data['r_c']
         u = self.coil_data['u']
         cpd = self.coil_data['cpd']
-        E_s_value = np.zeros(len(input_array))
-        E_s_direction = np.zeros_like(input_array)
+        if isinstance(input_array[0], np.ndarray):
+            num_of_coords = len(input_array[0])
+            E_s_value = np.zeros(len(input_array))
+            E_s_direction = np.zeros_like(input_array)
 
-        for i in range(len(input_array)):
-            x = input_array[i, 0]*10**-6
-            y = input_array[i, 1]*10**-6
+            for i in range(len(input_array)):
+                x = input_array[i, 0] * 10**-6
+                y = input_array[i, 1] * 10**-6
+                theta = m.atan2(y, x)
+                if num_of_coords == 3:
+                    z = abs(input_array[i, 2]) * 10**-6
+                    E_s_direction[i] = np.array([-m.sin(theta), m.cos(theta), 0])
+                else:
+                    z = cpd
+                    E_s_direction[i] = np.array([-m.sin(theta), m.cos(theta)])
+                rho = m.sqrt(x**2 + y**2)
+                if rho == 0:
+                    rho += 1 * 10**-18
 
+                m_kk = self._calc_m_kk(r_c, rho, z)
+                E_s_value[i] = self._calc_E_s(N, r_c, u, rho, m_kk)
+
+        elif isinstance(input_array[0], float):
+            num_of_coords = len(input_array)
+            x = input_array[0] * 10**-6
+            y = input_array[1] * 10**-6
             theta = m.atan2(y, x)
             if num_of_coords == 3:
-                z = abs(input_array[i, 2])*10**-6
-                E_s_direction[i] = np.array([-m.sin(theta), m.cos(theta), 0])
+                z = abs(input_array[2]) * 10**-6
+                E_s_direction = np.array([-m.sin(theta), m.cos(theta), 0])
             else:
                 z = cpd
-                E_s_direction[i] = np.array([-m.sin(theta), m.cos(theta)])
-
+                E_s_direction = np.array([-m.sin(theta), m.cos(theta)])
             rho = m.sqrt(x**2 + y**2)
             if rho == 0:
                 rho += 1 * 10**-18
 
             m_kk = self._calc_m_kk(r_c, rho, z)
-            E_s_value[i] = self._calc_E_s(N, r_c, u, rho, m_kk)
+            E_s_value = self._calc_E_s(N, r_c, u, rho, m_kk)
 
         return E_s_value, E_s_direction
 
-    def _create_positional_data_single_sec(self):
+    def _create_comp_data(self):
         self.comp_coords[0] = np.array([self.cell.x[0, 0], self.cell.y[0, 0], self.cell.z[0, 0]])
         for i in range(self.cell.totnsegs):
             self.comp_coords[i + 1] = np.array([self.cell.x[i, 1], self.cell.y[i, 1], self.cell.z[i, 1]])
             self.comp_vecs[i] = self.comp_coords[i + 1] - self.comp_coords[i]
 
+    def _create_c_comp_data_single_sec(self):
         self.c_comp_coords[0] = (self.comp_coords[0] + self.comp_coords[1]) * 0.5
         for i in range(self.cell.totnsegs - 1):
             self.c_comp_coords[i + 1] = (self.comp_coords[i + 1] + self.comp_coords[i + 2]) * 0.5
@@ -154,28 +273,27 @@ class MagneticField:
         return 1./r_a * E_value * np.dot(c_vec, E_dir) * 1000
 
     def _calc_input_current_ctc(self):
-        r_a = self.r_a
-        E_s_value, E_dir = self._spatial_field(self.c_comp_coords)
+        between_comps_coord = self.comp_coords[1:-1]
+        E_s_value, E_dir = self._spatial_field(between_comps_coord)
         for seg_idx in range(self.cell.totnsegs - 1):
             c_vec = self.c_comp_vecs[seg_idx]
-            E_s_c = (E_s_value[seg_idx] + E_s_value[seg_idx + 1]) * 0.5
             for i in range(len(self.time_array)):
-                E_value = E_s_c * self.E_t[i]
-                I_a = self._calc_I_a_ctc(E_value, E_dir[seg_idx], c_vec * 10**-6, r_a[seg_idx + 1])
+                E_value = E_s_value[seg_idx] * self.E_t[i]
+                I_a = self._calc_I_a_ctc(E_value, E_dir[seg_idx], c_vec * 10**-6, self.r_a[seg_idx + 1])
                 self.input_current[seg_idx, i] += -I_a
                 self.input_current[seg_idx + 1, i] += I_a
 
     def make_input_currents(self, method='ctc', multi_sec=False):
+        self._create_comp_data()
         if not multi_sec:
-            self._create_positional_data_single_sec()
+            self._create_c_comp_data_single_sec()
             # Method: ctc = Center of parent To Center of child (parent = previous comp, child = current comp)
             if method == 'ctc':
                 self._calc_input_current_ctc()
-            # Method: ste = Start of comp To End of comp
-            elif method == 'ste':
-                print('Not yet implemented')
         else:
-            print('Not yet implemented')
+            self._create_cell_structure_dicts()
+            self._calc_multisec_data()
+            self._calc_input_current_multisec()
 
     def insert_im_neuron(self):
         input_vec = []
@@ -184,7 +302,7 @@ class MagneticField:
         seg_idx = 0
         for sec in self.cell.allseclist:
             for seg in sec:
-                noise_vec = neuron.h.Vector(self.input_current[seg_idx])
+                noise_vec = neuron.h.Vector(-self.input_current[seg_idx])
                 syn = neuron.h.ISyn(seg.x, sec=sec)
                 syn.dur = 1e9
                 syn.delay = 0
@@ -196,9 +314,10 @@ class MagneticField:
         return input_vec, synlist
 
     def _calc_dv_mem(self, Em, Rm, Ra, Cm, v_prev_seg, v_curr_seg, v_next_seg, d, l, I_a):
+        # Returns V from (V/(Ohm*m) * (s/(F/m**2))) and (A/m**2) * (s/(F/m**2))
         return ((Em - v_curr_seg) / Rm
                 + (d / (4 * Ra)) * ((v_next_seg - v_curr_seg) / l**2 + (v_prev_seg - v_curr_seg) / l**2)
-                - I_a / (m.pi * d * l)) * ((self.timestep * 10**-3) / Cm)
+                + I_a / (m.pi * d * l)) * ((self.timestep * 10**-3) / Cm)
 
     def simulate_cell(self):
         v_mem = np.zeros((self.cell.totnsegs, len(self.time_array)))
@@ -251,17 +370,30 @@ class MagneticField:
 
     def plot_vmem(self, vmem, filename):
         visual_prestart = (self.time_array[-1] - self.time_array[0]) * 0.05
-        for seg_idx in range(self.cell.totnsegs):
-            plt.plot(self.time_array, vmem[seg_idx], label='seg '+str(seg_idx+1))
+        for seg_idx in range(len(vmem[:, 0])):
+            plt.plot(self.time_array, vmem[seg_idx], label=str(seg_idx+1))
         plt.xlim(self.time_array[0] - visual_prestart, self.time_array[-1])
-        plt.title('$Membrane Potential$')
+        plt.title('Membrane Potential')
         plt.xlabel('Time [ms]')
         plt.ylabel('V [mV]')
-        plt.legend()
+        plt.legend(loc='upper right', title='Seg. #')
         plt.savefig(join(filename))
         plt.clf()
 
-    def _plot_heatmap(self, data, x_array, y_array, filename, title, v_min, v_max, spat_res):
+    def plot_vmem_normalized(self, vmem, filename, rlc_type):
+        amplitude = np.max(vmem)
+        visual_prestart = (self.time_array[-1] - self.time_array[0]) * 0.05
+        for seg_idx in range(self.cell.totnsegs):
+            plt.plot(self.time_array, vmem[seg_idx] / amplitude, label=str(seg_idx+1))
+        plt.xlim(self.time_array[0] - visual_prestart, self.time_array[-1])
+        plt.title('Membrane Potential at $P_t$')
+        plt.xlabel('Time [ms]')
+        plt.ylabel('Normalized V')
+        plt.legend(loc='upper right', title='Seg. #')
+        plt.savefig(join(rlc_type + '_' + filename))
+        plt.clf()
+
+    def _plot_heatmap(self, data, x_array, y_array, filename, title, v_min, v_max, spat_res, cbar_label='[$Vs/Am$]'):
         # Color-maps: x->columns, y->rows, transpose the data array to make it fit for cartesian coordinates (array.T)
         # Useful cmaps: PuOr, PRGn, BrBG
         # Coil is only correctly places in the plot for symmetric axes with 0 as center coordinate
@@ -273,7 +405,7 @@ class MagneticField:
         pcm = plt.pcolormesh(data.T, cmap='BrBG', vmin=v_min, vmax=v_max)
         ax.add_patch(coil)
         coil.set_label('Coil')
-        plt.colorbar(pcm, label='[$Vs/Am$]')
+        plt.colorbar(pcm, label=cbar_label)
         plt.title(title)
         plt.xticks(np.linspace(0, len(x_array), 9), np.arange(-4, 4 + 1, 1))
         plt.yticks(np.linspace(0, len(y_array), 9), np.arange(-4, 4 + 1, 1))
@@ -299,11 +431,81 @@ class MagneticField:
         E_s_x = E_s_values * E_s_directions[:, :, 0]
         E_s_y = E_s_values * E_s_directions[:, :, 1]
         E_s = np.sqrt(E_s_x**2 + E_s_y**2)
+        E_amp_max = E_s * abs(np.max(self.E_t))
         v_max = np.max(E_s)
         v_min = -v_max
         self._plot_heatmap(E_s_x, x_array, y_array, 'E_s_x_heatmap', '$E_{s_x}$', v_min, v_max, spat_res)
         self._plot_heatmap(E_s_y, x_array, y_array, 'E_s_y_heatmap', '$E_{s_y}$', v_min, v_max, spat_res)
         self._plot_heatmap(E_s, x_array, y_array, 'E_s_heatmap', '$|E_s|$', v_min, v_max, spat_res)
+        v_max_amp = np.max(E_amp_max)
+        v_min_amp = -v_max_amp
+        self._plot_heatmap(E_amp_max, x_array, y_array, 'E_amp_max_heatmap_' + self.rlc_type, 'Maximum $|E|$',
+                           v_min_amp, v_max_amp, spat_res, cbar_label='[$V/m$]')
+
+    def plot_neuron_placement(self):
+        r_c = self.coil_data['r_c']
+        coil = plt.Circle((0, 0), r_c*10**2, color='gray', fill=False, lw=2)
+        fig, ax = plt.subplots()
+        for i in range(self.cell.totnsegs):
+            ax.plot(self.cell.x[i]*10**-4, self.cell.y[i]*10**-4, 'o', linestyle='-')
+        ax.add_patch(coil)
+        plt.title('Placement')
+        plt.xlabel('x [cm]')
+        plt.ylabel('y [cm]')
+        xmax = np.max(self.cell.x)*10**-4
+        xmin = np.min(self.cell.x)*10**-4
+        ymax = np.max(self.cell.y)*10**-4
+        ymin = np.min(self.cell.y)*10**-4
+        if xmax == xmin:
+            xmax += 0.5
+            xmin -= 0.5
+        else:
+            xmax += (xmax-xmin)*0.30
+            xmin -= (xmax-xmin)*0.30
+        if ymax == ymin:
+            ymax += 0.5
+            ymin -= 0.5
+        else:
+            ymax += (ymax-ymin)*0.30
+            ymin -= (ymax-ymin)*0.30
+        plt.xlim(xmin, xmax)
+        plt.ylim(ymin, ymax)
+        plt.savefig(join('neuron_placement'))
+        plt.clf()
+
+    def plot_neuron_placement_multisec(self):
+        r_c = self.coil_data['r_c']
+#        coil = plt.Circle((0, 0), r_c*10**2, color='gray', fill=False, lw=2)
+        fig, ax = plt.subplots()
+        for sec in self.cell.allseclist:
+            sec_name = sec.name()
+            sec_idx = self.cell_idx_dict[sec_name]
+            for seg_idx in sec_idx:
+                ax.plot(self.cell.x[seg_idx]*10**-4, self.cell.y[seg_idx]*10**-4, 'o', linestyle='-')
+#        ax.add_patch(coil)
+        plt.title('Placement')
+        plt.xlabel('x [cm]')
+        plt.ylabel('y [cm]')
+        xmax = np.max(self.cell.x)*10**-4
+        xmin = np.min(self.cell.x)*10**-4
+        ymax = np.max(self.cell.y)*10**-4
+        ymin = np.min(self.cell.y)*10**-4
+        if xmax == xmin:
+            xmax += 0.5
+            xmin -= 0.5
+        else:
+            xmax += (xmax-xmin)*0.30
+            xmin -= (xmax-xmin)*0.30
+        if ymax == ymin:
+            ymax += 0.5
+            ymin -= 0.5
+        else:
+            ymax += (ymax-ymin)*0.30
+            ymin -= (ymax-ymin)*0.30
+        plt.xlim(xmin, xmax)
+        plt.ylim(ymin, ymax)
+        plt.savefig(join('neuron_placement'))
+        plt.clf()
 
 if __name__ == "__main__":
     pass
--- test_magnetic_field_new.py
+++ test_magnetic_field_new.py
@@ -11,15 +11,16 @@ from magnetic_field_new import MagneticField
 
 time_start = 0
 ts = 0.01
-time_stop = 5
+time_stop = 3
 number_of_ts = int((time_stop - time_start) / ts)
 time_array = np.linspace(time_start, time_stop, number_of_ts)
 
+hoc_file = 'branching_Y_axon.hoc'
+
 cell_parameters = {
-        'morphology': '3comp_axon.hoc',
+        'morphology': hoc_file,
         'v_init': -70,
         'nsegs_method': None,
-        "lambda_f": 100.,
         "tstart": time_start,
         "tstop": time_stop,
         "dt": ts,
@@ -30,19 +31,24 @@ cell_parameters = {
                                "e_pas": -70},
 }
 cell = LFPy.Cell(**cell_parameters)
-cell.set_pos(x=-2 * 1e4, y=1 * 1e4, z=-1 * 1e4)
+cell.set_pos(x=0, y=20000, z=-10000)
 
-MagField = MagneticField(time_array, cell=cell)
-MagField.make_input_currents()
+rlc_type = 'over'
+MagField = MagneticField(time_array, cell=cell, rlc_type=rlc_type)
+MagField.make_input_currents(multi_sec=True)
 
-v_mem = MagField.simulate_cell()
-MagField.plot_vmem(v_mem, "manual_v_mem.png")
+# v_mem = MagField.simulate_cell()
+# MagField.plot_vmem(v_mem, "manual_v_mem.png")
+# MagField.plot_vmem_normalized(v_mem, "manual_v_mem_norm.png", rlc_type)
 
 input_vec, syn = MagField.insert_im_neuron()
 cell.simulate(rec_vmem=True, rec_imem=True)
-MagField.plot_vmem(cell.vmem[:, :-1], "neuron_v_mem.png")
+v_mem_neur = cell.vmem[:, :-1]
+MagField.plot_vmem(v_mem_neur, "neuron_v_mem.png")
+
+MagField.plot_neuron_placement_multisec()
 
 # MagField.plot_I_coil()
-# print('Maximum magnetic field magnitude =', MagField.magnetic_field_magnitude, 'Tesla in the center of the coil.')
+print('Maximum magnetic field magnitude =', MagField.magnetic_flux_density, 'Tesla in the center of the coil.')
 # MagField.plot_E_t()
 # MagField.plot_E_s_heatmaps()
